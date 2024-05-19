8ae1a618e7a1d77c53b0abd76a38073944668c2b
Genesis McGruder <genesismcgruder@gmail.com>
2024-05-19 05:14:37 -0400

    added checks for Book and Genre to the debug.py file

--- lib/debug.py
+++ lib/debug.py
@@ -4,14 +4,32 @@ import ipdb
 
 
 def reset_database():
+    Genre.drop_table()
+    Book.drop_table()
     User.drop_table()
     User.create_table()
+    Book.create_table()
+    Genre.create_table()
+    
 
 
-    karen = User.create("Karen")
-    leroy = User.create("Leroy")
-    lucy = User.create("Lucy")
-    herold = User.create("Herold")
+    karen = User.create("Karen", 1234)
+    leroy = User.create("Leroy", 5678)
+    lucy = User.create("Lucy", 1616)
+    herold = User.create("Herold", 1999)
+
+    bible = Book.create("Bible AMP", "God", 1408, 3, 1)
+    living_fearless = Book.create("Living Fearless", "Jamie Winship", 170, 1, 1)
+    kingdom_culture = Book.create("Kingdom Culture", "Dan Farrelly", 365, 2, 1)
+    the_way_of_life = Book.create("The Way of Life", "Bill Johnson", 224, 4, 1)
+
+
+    religion = Genre.create("Religion")
+    fiction= Genre.create("Fiction")
+    memoir = Genre.create("Memoir")
+    historical = Genre.create("Historical")
+
+    
 
 
 reset_database()
